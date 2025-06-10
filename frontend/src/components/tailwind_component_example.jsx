f5878055afc262de0adabb45fe00ff077836afb9
Shawn Hank <shawn@shawnhank.com>
2025-06-10 11:10:46 -0600

    added tailwind specific updates to project.

---
+++ frontend/src/components/tailwind_component_example.jsx
@@ -0,0 +1,16 @@
+export default function Example() {
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4">
+      <div className="md:flex">
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            NomNomLog
+          </div>
+          <p className="mt-2 text-slate-500">
+            Track your meals and discover new restaurants.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
