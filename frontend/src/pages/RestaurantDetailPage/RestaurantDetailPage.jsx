41e39d9927a446df661fd912b95a5b1bc0c1bcf0
Shawn Hank <shawn@shawnhank.com>
2025-06-13 05:11:20 -0600

    updated retaurant detail page to use max-w-4xl (896px) max width

--- frontend/src/pages/RestaurantDetailPage/RestaurantDetailPage.jsx
+++ frontend/src/pages/RestaurantDetailPage/RestaurantDetailPage.jsx
@@ -104,7 +104,7 @@ export default function RestaurantDetailPage() {
     : null;
 
   return (
-    <div className="max-w-3xl mx-auto p-4 sm:p-6">
+    <div className="max-w-4xl mx-auto p-4 sm:p-6">
       {/* Breadcrumbs */}
       <div className="mb-4">
         <SimpleBreadcrumbs 
@@ -126,10 +126,16 @@ export default function RestaurantDetailPage() {
           />
         </div>
         <div className="flex space-x-2 items-center">
+          <Link to="/restaurants/new">
+            <Button positive className="flex items-center">
+              <PlusIcon className="w-5 h-5 mr-1" />
+              <span>Add Restaurant</span>
+            </Button>
+          </Link>
           <Link to={`/restaurants/${restaurant._id}/edit`}>
             <Button positive>Edit</Button>
           </Link>
-          <Button 
+          <Button
             negative
             onClick={() => setShowDeleteModal(true)}
           >
